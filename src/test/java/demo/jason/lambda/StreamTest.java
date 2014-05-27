a2fe2e9f9e0fe07eb7554a7182623c21bccec335
smilingjason <smilingjason@gmail.com>
2014-05-27 08:00:59 +0800

    add test cases for lazy and short-cutting operations

--- src/test/java/demo/jason/lambda/StreamTest.java
+++ src/test/java/demo/jason/lambda/StreamTest.java
@@ -5,7 +5,7 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 import java.util.stream.*;
 import java.util.*;
-
+import java.util.concurrent.atomic.*;
 /**
  * @Author Jason Huang
  **/
@@ -57,11 +57,11 @@ public class StreamTest {
         long end = System.nanoTime();
         System.out.println("sequential: " + (end - start) / 1.0e6);
         assertEquals(threadSet.size(), 1);      
-        //then, test with parellel
+        //then, test with parallel
         threadSet.clear();
         start = System.nanoTime();
         Arrays.stream(a)
-              .parallel()
+              .parallel() //use a parallel stream
               .map(v -> {
                   //save the thread id 
                   threadSet.add(Thread.currentThread().getId());
@@ -72,5 +72,47 @@ public class StreamTest {
         System.out.println("parallel: " + (end - start) / 1.0e6 
                 + " with thread count: " + threadSet.size());
         assertTrue(threadSet.size() > 1);      
+    }
+
+    public void testOperationLaziness() {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        AtomicInteger ai = new AtomicInteger(0);
+        Stream<Integer> stream = list.stream();
+        Stream<Integer> mappedStream =stream.map(v -> {
+            ai.incrementAndGet();
+            return v * 3; 
+        });
+        assertEquals(ai.get(), 0); //map is a lazy operation
+
+        Stream<Integer> filteredStream = mappedStream.filter(v -> {
+            ai.incrementAndGet();
+            return (v % 2) == 0; 
+        });
+        assertEquals(ai.get(), 0); //filter is a lazy operation
+
+        //stream.count() is not lazy op it's a terminal op.
+        //when it's called, all the lazy ops will be fused and executed
+        assertEquals(filteredStream.count(), 2); 
+        assertEquals(ai.get(), list.size() * 2);
+    }
+    
+    //this test case shows the fusing and short-cutting of stream ops
+    public void testShortcutOperation() {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        AtomicInteger ai = new AtomicInteger(0);
+        Stream<Integer> stream = list.stream();
+        Stream<Integer> filteredStream = stream.filter(v -> {
+            ai.incrementAndGet();
+            return (v % 2) == 0; 
+        });
+        java.util.Optional<Integer> r = filteredStream.findFirst();
+        //first even number is 2 
+        assertEquals(r.get().intValue(), 2);
+        //filter is a lazy op and it's fused with findFirst which is short-cutting
+        //terminal op. So, filter operation won't execute for all data elements.
+        //In this case, it will stop at 2nd element which matches the findFirst.
+        //This will help on performance.
+        assertEquals(ai.get(), 2);
+
     }
 }
